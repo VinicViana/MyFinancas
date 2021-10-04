695a8d038e233e2b65b225c4b17df3b769d46ec0
Vinicius Viana Oliveira Soares <vvosoares@hotmail.com>
2021-10-03 21:59:45 -0300

    Criado o projeto MyFinancas.VIEW e implementado as views principais

---
+++ MyFinancas.VIEW/Views/controlador2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFinancas.VIEW.Views
+{
+    /// <summary>
+    /// Interaction logic for controlador2.xaml
+    /// </summary>
+    public partial class controlador2 : UserControl
+    {
+        public controlador2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyFinancas.VIEWS/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MyFinancas.VIEWS.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFinancas.VIEWS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ResumoVM();
+        }
+        private void btnResumo_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new ResumoVM();
+        }
+
+        private void btnGanhos_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new GanhosVM();
+        }
+
+        private void btnDespesas_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new DespesasVM();
+        }
+    }
+}
